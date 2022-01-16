ac160e2ab83991e6a83c94c9fb93f39cb5b67485
fumi uk <dcfumiya.238@gmail.com>
2022-01-17 08:51:58 +0900

    react-basic

---
+++ src/components/Article.jsx
@@ -0,0 +1,12 @@
+import { Content, Title } from "./index";
+
+const Article = (props) => {
+  return (
+    <div>
+      <Title title={props.title} />
+      <Content content={props.content} />
+    </div>
+  )
+};
+
+export default Article;
